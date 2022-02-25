e1223e182b2f19f67b988463b40ff5b421f034f7
Devin Hill <33045257+iamdevinhill@users.noreply.github.com>
2022-02-25 06:32:04 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const question = new Map([
+  ['question', 'what is best program language?'],
+  [1, 'javascript'],
+  ['correct', 1],
+  [true, 'correct'],
+  [false, 'nope']
+])
+
+console.log(question)
